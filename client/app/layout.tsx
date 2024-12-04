157415897c1523b474684ff95e79ece8d936c2c2
aliskhangazamatov <ag@farcana.com>
2024-12-04 22:06:18 +0300

    fix

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import { ThemeProvider } from "next-themes";
 import { MantineProvider } from "@mantine/core";
 const inter = Inter({ subsets: ["latin"] });
+import { Notifications } from "@mantine/notifications";
 
 export const metadata: Metadata = {
   title: "Aliskhan's portfolio",
@@ -14,14 +15,17 @@ export default function RootLayout({ children }: any) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
-        >
-          <MantineProvider>{children}</MantineProvider>
-        </ThemeProvider>
+        <MantineProvider>
+          <Notifications className=" absolute bottom-10 right-3 bg-amber-50" />
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </MantineProvider>
       </body>
     </html>
   );
