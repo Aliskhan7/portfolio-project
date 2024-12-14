cbf9e22d504efeb01ef3674ef49626e6753c1ffd
aliskhangazamatov <ag@farcana.com>
2024-12-14 08:07:08 +0300

    fix switcher local

--- client/i18n.ts
+++ client/i18n.ts
@@ -1,20 +1,22 @@
-import i18next, { use } from "i18next";
 import { initReactI18next } from "react-i18next";
+import en from "./public/locales/en/en.json";
+import ru from "./public/locales/ru/ru.json";
+import i18n from "i18next";
 
-i18next.use(initReactI18next).init({
-  lng: "en", // if you're using a language detector, do not define the lng option
-  debug: true,
-  fallbackLng: "en",
+const locale: string | null = localStorage.getItem("lang");
+i18n.use(initReactI18next).init({
+  resources: {
+    en: {
+      translation: en,
+    },
+    ru: {
+      translation: ru,
+    },
+  },
+  lng: locale || "ru",
+  fallbackLng: "ru",
   interpolation: {
     escapeValue: false,
   },
-  // resources: {
-  //   en: {
-  //     main_title: "hello world",
-  //   },
-  //   ru: {
-  //     main_title: "аываыаыв",
-  //   },
-  // },
 });
-export default i18next;
+export default i18n;
